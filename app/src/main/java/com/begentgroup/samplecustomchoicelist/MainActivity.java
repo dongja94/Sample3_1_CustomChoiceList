62b3342ff33c54410bf8b83fe251a5225af37c75
dongja94 <dongja94@gmail.com>
2016-04-20 11:49:32 +0900

    Initial commit

---
+++ app/src/main/java/com/begentgroup/samplecustomchoicelist/MainActivity.java
@@ -0,0 +1,35 @@
+package com.begentgroup.samplecustomchoicelist;
+
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    PersonAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView)findViewById(R.id.listView);
+        mAdapter = new PersonAdapter();
+        listView.setAdapter(mAdapter);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        initData();
+    }
+
+    private void initData() {
+        for (int i = 0 ; i < 40 ; i++) {
+            Person p = new Person();
+            p.setName("item" + i);
+            p.setAge(i);
+            p.setPhoto(ContextCompat.getDrawable(this, R.mipmap.ic_launcher));
+            mAdapter.add(p);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/begentgroup/samplecustomchoicelist/PersonAdapter.java
@@ -0,0 +1,86 @@
+package com.begentgroup.samplecustomchoicelist;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by dongja94 on 2016-04-20.
+ */
+public class PersonAdapter extends BaseAdapter implements PersonView.OnImageClickListener {
+
+    List<Person> items = new ArrayList<Person>();
+
+    public void add(Person p) {
+        items.add(p);
+        notifyDataSetChanged();
+    }
+
+    public void remove(Person p) {
+        items.remove(p);
+        notifyDataSetChanged();
+    }
+
+    public void addAll(List<Person> items) {
+        this.items.addAll(items);
+        notifyDataSetChanged();
+    }
+
+    public void clear() {
+        items.clear();
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        PersonView view;
+        if (convertView == null) {
+            view = new PersonView(parent.getContext());
+            view.setOnImageClickListener(this);
+        } else {
+            view = (PersonView)convertView;
+        }
+        view.setPerson(items.get(position));
+        view.setOnImageClickListener(imageClickListener);
+        return view;
+    }
+
+    PersonView.OnImageClickListener imageClickListener;
+    public void setOnImageClickListener(PersonView.OnImageClickListener listener) {
+        imageClickListener = listener;
+    }
+
+    @Override
+    public void onImageClick(PersonView view, Person person) {
+        if (mListener != null) {
+            mListener.onItemImageClick(this, view, person);
+        }
+    }
+
+    public interface OnAdapterItemClickListener {
+        public void onItemImageClick(PersonAdapter apdater, PersonView view, Person person);
+    }
+
+    OnAdapterItemClickListener mListener;
+    public void setOnAdapterItemClickListener(OnAdapterItemClickListener listener) {
+        mListener = listener;
+    }
+}
